259d1520c9eea993cf2cb70cda64f8e383bf20bb
mhkim <mhkim@antlinesoft.com>
2020-06-16 13:20:32 +0900

    eee

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import './App.css';
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, NavLink} from 'react-router-dom'
+import { Link } from '@material-ui/core';
 
 function App() {
+  const active = {color: "red"};
+
+   
   return (
     <BrowserRouter>
+      <nav>
+        <ul>
+          <li><NavLink exact={true} activeStyle={active} to="/">Home</NavLink></li>
+          <li><NavLink activeStyle={active} to="/dashboard">Dashboard</NavLink></li>
+        </ul>
+      </nav>
       <Switch>
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/" component={Home} />
         
       </Switch>
-    </BrowserRouter>
+    </BrowserRouter>    
   );
 }
 
